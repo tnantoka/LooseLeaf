c17fbcf043850e7b86b5c019a1b802c7239fd4df
tnantoka <bornneet@livedoor.com>
2011-10-15 23:15:42 +0900

    timeago, posts by serverside

--- skeleton/public/javascripts/script.js
+++ skeleton/public/javascripts/script.js
@@ -23,6 +23,36 @@ $(function () {
     $('html, body').animate({ scrollTop: 0 }, 'fast');
   });
 
+  // Get next entry when scroll bottom
+  var nowLoading;
+  var socket = io.connect('/posts');
+
+  socket.on('connect', function() {
+
+    socket.on('next', function(post) {
+      nowLoading = false;
+      if (post) {
+        var section = [ 
+          '<section>',
+            '<h1>', post.title, '</h1>',
+            '<p>', new Date(post.date), '</p>',
+            '<pre>', post.body, '</pre>',
+          '</section>'].join('');
+        document.body.innerHTML += section;
+      }   
+    }); 
+
+    onBottom(function() {
+      var offset = $('.page-header').length;
+      console.log(offset);
+      if (!nowLoading) {
+        nowLoading = true;
+        socket.emit('next', { offset: offset }); 
+      }   
+    }); 
+
+  }); 
+
   // syntax highlight
   //prettyPrint();
 })
